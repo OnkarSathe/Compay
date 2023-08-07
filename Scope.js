bfc496ab96b631f3e13f4c5c3dabfd0df95a930a
Onkar <onkarsathe939@gmail.com>
2023-08-07 12:55:29 +0530

    Scope Inital

---
+++ Scope.js
@@ -0,0 +1,16 @@
+//    console.log(myName);
+// }
+var myName="h";
+
+{
+ var myName="shubham";
+  console.log("Access in local scope success - "+myName);
+
+  if (true) {
+    const myName="Rahul";
+    console.log("Access in block scope success - "+myName);
+  }
+}
+// f1();
+
+console.log("Access in global scope success - "+myName);
